f6b840e0905a76fee2e06e7a3363cddbe778e0b1
imangadydv <148368906+imangadydv@users.noreply.github.com>
2025-03-16 16:35:35 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -32,6 +32,9 @@ app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
+app.get("/",(req,res)=>{
+    res.send("job portal backend is running");
+});
 
 
 app.listen(PORT,()=>{
